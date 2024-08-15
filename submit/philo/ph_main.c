8d7a71af454a79d4d93ade9c9485c0a35b83c7a0
PalmNeko <nitunamoni13579@gmail.com>
2024-08-15 11:15:53 +0900

    refactor: delete redundant code

--- submit/philo/ph_main.c
+++ submit/philo/ph_main.c
@@ -16,45 +16,43 @@
 #include <string.h>
 #include <errno.h>
 
-t_philosopher	*ph_generate_philosophers(int philo_cnt, t_manager *common);
-int				ph_start(t_philosopher *philos, int philo_cnt);
+t_philosopher	*ph_generate_philosophers(t_manager *manager);
+int				ph_start(t_manager *manager, t_philosopher *philos);
 
 int	ph_main(t_manager *manager)
 {
 	t_philosopher	*philos;
 	int				error;
 
-	philos = ph_generate_philosophers(manager->philo_cnt, manager);
+	philos = ph_generate_philosophers(manager);
 	if (philos == NULL)
 		return (ENOMEM);
 	manager->philos = philos;
 	gettimeofday(&manager->start, NULL);
 	manager->in_process = true;
 	pthread_mutex_init(&manager->lock, NULL);
-	error = ph_start(philos, manager->philo_cnt);
+	error = ph_start(manager, philos);
 	ph_destroy_philosophers(philos, manager->philo_cnt);
 	return (error);
 }
 
-int	ph_start(t_philosopher *philos, int philo_cnt)
+int	ph_start(t_manager *manager, t_philosopher *philos)
 {
 	pthread_t		*threads;
 	pthread_t		print_thread;
-	t_manager		*manager;
 	int				index;
 	int				error;
 
-	threads = (pthread_t *)malloc(sizeof(pthread_t) * philo_cnt);
+	threads = (pthread_t *)malloc(sizeof(pthread_t) * manager->philo_cnt);
 	if (threads == NULL)
 		return (ENOMEM);
-	error = ph_create_philo_threads(threads, philos, philo_cnt);
+	error = ph_create_philo_threads(threads, philos, manager->philo_cnt);
 	if (error != 0)
 		return (free(threads), error);
 	pthread_create(&print_thread, NULL,
-		(void *(*)(void *))ph_routine_print, philos[0].manager);
-	manager = philos[0].manager;
+		(void *(*)(void *))ph_routine_print, manager);
 	index = 0;
-	while (index < philo_cnt)
+	while (index < manager->philo_cnt)
 		pthread_join(threads[index++], NULL);
 	pthread_mutex_lock(&manager->lock);
 	manager->in_process = false;
@@ -64,20 +62,22 @@ int	ph_start(t_philosopher *philos, int philo_cnt)
 	return (0);
 }
 
-t_philosopher	*ph_generate_philosophers(int philo_cnt, t_manager *common)
+t_philosopher	*ph_generate_philosophers(t_manager *manager)
 {
 	t_philosopher	*philos;
 	int				index;
+	int				size;
 
-	philos = (t_philosopher *)malloc(sizeof(t_philosopher) * philo_cnt);
+	size = sizeof(t_philosopher) * manager->philo_cnt;
+	philos = (t_philosopher *)malloc(size);
 	if (philos == NULL)
 		return (NULL);
-	memset(philos, 0, sizeof(t_philosopher *) * philo_cnt);
+	memset(philos, 0, size);
 	index = 0;
-	while (index < philo_cnt)
+	while (index < manager->philo_cnt)
 	{
 		philos[index] = (t_philosopher){
-			.manager = common,
+			.manager = manager,
 			.no = index + 1,
 		};
 		pthread_mutex_init(&philos[index].fork, NULL);
