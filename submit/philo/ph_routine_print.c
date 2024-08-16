75e83e7e36252890c0c0dfe736ecd43a8a1fadac
PalmNeko <nitunamoni13579@gmail.com>
2024-08-16 16:24:02 +0900

    update: msleep to usleep

--- submit/philo/ph_routine_print.c
+++ submit/philo/ph_routine_print.c
@@ -24,7 +24,7 @@ void	*ph_routine_print(t_manager *manager)
 	while (manager->in_process)
 	{
 		if (manager->action_queue->size <= 1 && manager->in_process)
-			ph_msleep(0, manager);
+			usleep(0);
 		error = pthread_mutex_lock(&manager->action_queue->lock);
 		if (error != 0)
 			return (NULL);
