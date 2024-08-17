c075429a08822da7ba7b8a0025987ac6902d78f5
PalmNeko <nitunamoni13579@gmail.com>
2024-08-17 17:45:27 +0900

    refactor: replace function

--- submit/philo/ph_msleep.c
+++ submit/philo/ph_msleep.c
@@ -18,26 +18,11 @@
 int	ph_msleep(unsigned int msec, t_manager *manager)
 {
 	struct timeval	start;
-	struct timeval	now;
 	struct timeval	diff;
 	struct timeval	endtime;
-	bool			in_progress;
 
 	gettimeofday(&start, NULL);
 	diff = ph_msectotimeval(msec);
 	timeradd(&start, &diff, &endtime);
-	now = start;
-	gettimeofday(&now, NULL);
-	while(timercmp(&now, &endtime, <))
-	{
-		pthread_mutex_lock(&manager->lock);
-		in_progress = manager->in_process;
-		pthread_mutex_unlock(&manager->lock);
-		if (in_progress == false)
-			break;
-		if (ph_wait_once(&now, &endtime) == -1)
-			return (-1);
-		gettimeofday(&now, NULL);
-	}
-	return (0);
+	return (ph_tvsleep_until(&endtime, manager));
 }
